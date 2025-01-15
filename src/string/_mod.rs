831990d0a80abdb13d9c566846b13a7441bb7625
Esther Alter <alters@mit.edu>
2025-01-15 10:57:48 -0500

    default

--- src/string/_mod.rs
+++ src/string/_mod.rs
@@ -123,4 +123,10 @@ cfg_alloc! {
             Self(self.0.clone())
         }
     }
+
+    impl Default for String {
+        fn default() -> Self {
+            rust::String::default().into()
+        }
+    }
 }
